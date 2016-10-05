48f362ff8340d0537741bb9d06642a46344fea9d
Song2U <Song2U@Song2U-PC>
2016-10-05 09:24:21 +0900

    Reading Better 새로 생성

---
+++ sql/sequence.sql
@@ -0,0 +1,14 @@
+-- sequence
+DROP SEQUENCE seq_attend;
+
+CREATE SEQUENCE seq_attend
+START WITH 1
+INCREMENT BY 1;
+
+DROP SEQUENCE seq_member;
+
+CREATE SEQUENCE seq_member
+START WITH 1
+INCREMENT BY 1;
+
+commit;
